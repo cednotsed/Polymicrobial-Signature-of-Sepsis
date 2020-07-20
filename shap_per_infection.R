52d46a1004b05eb008eb1e2f68cd379567971839
Cedric <cedriccstan@gmail.com>
2020-07-20 06:05:51 +0100

    add abundance boxplot

--- shap_per_infection.R
+++ shap_per_infection.R
@@ -12,12 +12,29 @@ plt1  <- ggplot(df, aes(x = Genus, y = shap, color = color)) +
          geom_hline(yintercept = 0, linetype = "dotted") +
          labs(y = "SHAP Values", x = "Genus") +
          theme_bw() + 
+         theme(axis.title.x = element_blank(),
+               axis.text.x = element_blank(),
+               axis.ticks.x = element_blank(),
+               legend.position = "none")
+         
+
+## Boxplot of genera abundance
+df$abundance <- log(df$abundance)
+
+plt2  <- ggplot(df, aes(x = Genus, y = abundance)) +
+         geom_boxplot() +
+         labs(y = "ln(read count)", x = "Genus") +
+         geom_hline(yintercept = 0, linetype = "dotted") +
+         theme_bw() +
          theme(axis.ticks.x = element_blank(),
                axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5),
                legend.position = "none")
 
-## Boxplot of genera abundance
-df2 <- read.csv("datasets/karius_genus_raw_maxi.csv", sep = ",", stringsAsFactors = F)
-genera <- unique(df$Genus)
-df2 <- df2[]
-# ggsave("results/shap_by_infection.png", dpi = 600, width = 6, height = 4)
+require(ggpubr)         
+ggarrange(plt1, plt2, 
+          ncol = 1, nrow = 2, 
+          common.legend = T, 
+          align = "v",
+          labels = "auto",
+          label.y = 1.1)
+ggsave("results/shap_by_infection.png", dpi = 600, width = 6, height = 5)
