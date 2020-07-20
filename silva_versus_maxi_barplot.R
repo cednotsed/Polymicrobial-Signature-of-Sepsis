015d6c5cade7ccc70cf5b234166ff57dae6a0ed4
Cedric <cedriccstan@gmail.com>
2020-07-20 03:04:50 +0100

    Compare databases

--- silva_versus_maxi_barplot.R
+++ silva_versus_maxi_barplot.R
@@ -33,13 +33,15 @@ means <- aggregate(melted_maxi$value, by=list(melted_maxi$variable), mean)
 orders <- as.character(means$Group.1)[order(means$x, decreasing = T)][1:8]
 
 melted_maxi <- melt(maxi[, colnames(maxi) %in% c("Group.1", orders)])
-
-# Silva db
 melted_silva <- melt(silva[, colnames(silva) %in% c("Group.1", orders)])
+
+# Set factor sequence
+melted_maxi$variable <- factor(melted_maxi$variable, levels = orders)
+melted_silva$variable <- factor(melted_silva$variable, levels = orders)
 
 plt1 <- ggplot(melted_maxi, aes(y = value, x=Group.1, fill=variable)) +
   geom_bar(stat="identity") +
-  labs(fill = "Order", x = "MaxiKraken", y = "Mean Relative Abundance (%)") +
+  labs(fill = "Order", x = "MaxiKraken2", y = "Mean Relative Abundance (%)") +
   scale_fill_manual(values = pal)
 
 plt2 <- ggplot(melted_silva, aes(y = value, x=Group.1, fill=variable)) +
@@ -50,6 +52,28 @@ plt2 <- ggplot(melted_silva, aes(y = value, x=Group.1, fill=variable)) +
   labs(fill = "Order", x = "Silva") +
   scale_fill_manual(values = pal)
 
+# Plot no. of minimisers in database
+silva <- read.csv("silva_290620_info.txt", sep = "\t", stringsAsFactors = F, header = F)
+maxi <- read.csv("maxikraken2_info.txt", sep = "\t", stringsAsFactors = F, header = F)
+
+# Trim whitespace
+silva$V6 <- trimws(silva$V6)
+maxi$V6 <- trimws(maxi$V6)
+
+# Get orders
+silva_df <- silva[silva$V6 %in% orders, c("V1", "V6")]
+silva_df$Database <- "Silva"
+maxi_df <- maxi[maxi$V6 %in% orders, c("V1", "V6")]
+maxi_df$Database <- "MaxiKraken2"
+
+plot_df <- rbind(silva_df, maxi_df)
+plot_df$V6 <- factor(plot_df$V6, levels = orders)
+
+plt3 <- ggplot(plot_df, aes(y = V1, x = Database, fill = V6)) +
+        geom_bar(stat="identity") +
+        labs(fill = "Order", x = "Database", y = "Proportion of minimisers (%)") +
+        scale_fill_manual(values = pal)
+
 require(ggpubr)
-ggarrange(plt1, plt2, ncol = 2, nrow = 1, common.legend = T, align = "hv")
+ggarrange(plt1, plt2, plt3, ncol = 2, nrow = 2, common.legend = T, align = "hv")
 ggsave("../results/silva_versus_maxi_barplot.png", dpi = 600, width = 8, height = 5)
